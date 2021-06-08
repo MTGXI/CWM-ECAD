246217abeeea5205ce5f8afd68344016fce9f5d7
Cloud User <centos@ip-172-31-3-169.eu-west-2.compute.internal>
2021-06-08 15:09:06 +0000

    first attempt

--- Ex5/top.v
+++ Ex5/top.v
@@ -11,5 +11,29 @@
 //
 //  outputs:
 //           heating, cooling
+//				both cannot be on at same time
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 100ps
 
+module AC (clk,temperature,heating,cooling);
+
+	//adding ports
+	input clk;
+	input [4:0] temperature;
+	output heating,cooling;
+
+	reg [1:0] state;
+
+	//let left bit be heating, let no bits be idle, let right be cooling - 11 is not possible
+	assign heating = (state[1]);
+	assign cooling = (state[0]);
+
+	always @(posedge clk) begin
+		case(state)
+			0: state <= temperature<20 ? 2'b10 : 2'b00;//for heating state - if temp<20 state = 10 otherwise goes to idle
+			1: state <= temperature<=18 ? 2'b10 : temperature>=22 ? 2'b01 : 2'b00;//for idle state - if temp <= 18 state goes to 10 etc, stays at idle if neit
+			2: state <= temperature>20 ? 2'b01 : 2'b00; 
+			default: state <= 2'b00;//default state is idle
+		endcase
+	end
+endmodule
--- Ex5/top_tb.v
+++ Ex5/top_tb.v
@@ -6,4 +6,74 @@
 // Description: A testbench module to test Ex5 - Air Conditioning
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 100ps
 
+module top_tb(
+	);
+
+	//parameters
+	parameter CLK_PERIOD = 10;
+
+	//registers
+	reg clk,err;
+	reg [4:0] temperature;
+	reg [1:0] prev_state;
+	reg [1:0] state;
+	wire heating,cooling;
+	
+
+	//creating the clock
+	initial begin
+       	clk = 1'b0;
+		forever
+        #(CLK_PERIOD/2) clk=~clk;
+   	end
+
+	//logic and testing output
+	
+	initial begin
+		err=0;
+		//for instance, start in idle with initial temp of 15
+		temperature = 5'b01111;
+		state = 2'b00;
+		
+		prev_state = state;
+		//increment temp each time to see state changes
+		forever begin
+			#CLK_PERIOD;
+			if ((temperature<20)&&(state != 2'b10)) begin
+				$display("***TEST FAILED 1***");
+				err = 1;
+			end
+			else if ((temperature>22)&&(state != 2'b01)) begin
+				$display("***TEST FAILED 2***");
+				err = 1;
+			end
+			if ((prev_state != state)&&(state != 2'b00)) begin
+				$display("***TEST FAILED 3***");
+				err = 1;
+			end
+		temperature = temperature + 5'd1;
+		prev_state = state;
+		end
+
+	end
+
+	//end sim block
+	initial begin
+        #100 
+        if (err==0)
+          $display("***TEST PASSED***");
+        $finish;
+      end
+
+
+	//instantiation
+	AC top (
+	.clk (clk),
+	.temperature (temperature),
+	.heating (heating),
+	.cooling (cooling)
+	);
+
+endmodule
