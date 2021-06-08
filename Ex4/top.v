123efb6a57fb522e2d217940bf1b7a705e42a27b
Cloud User <centos@ip-172-31-3-169.eu-west-2.compute.internal>
2021-06-08 09:54:07 +0000

    first stage - SE

--- Ex4/top.v
+++ Ex4/top.v
@@ -16,3 +16,35 @@
 //  You need to write the whole file.
 //////////////////////////////////////////////////////////////////////////////////
 
+`timescale 1ns / 100ps
+
+module lights (rst,clk,button,colour);
+
+	//adding ports
+	input rst,clk,button;
+	output reg [2:0] colour;
+
+	//no register required for colour
+
+	always @ (posedge clk) begin
+		if(rst) begin
+			colour = 3'd0;
+		end
+		else begin
+			if ((colour==3'd0)||(colour==3'd7)) begin
+				#1;
+				colour = 3'd1;
+			end
+			else begin
+				if (button==1'b1) begin
+					#1;
+					colour = colour + 3'd1;
+				end
+				else begin
+				end
+			end
+		end
+	end
+
+endmodule
+			
--- Ex4/top_tb.v
+++ Ex4/top_tb.v
@@ -6,4 +6,40 @@
 // Description: A testbench module to test Ex4 - Dynamic LED lights
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 100ps
 
+module top_tb(
+	);
+
+	//parameters
+	parameter CLK_PERIOD = 10;
+
+	//registers
+	reg clk,rst,err;
+	wire [2:0] colour;
+	
+
+	//creating the clock
+	initial begin
+       clk = 1'b0;
+       forever
+         #(CLK_PERIOD/2) clk=~clk;
+     end
+
+	//logic and testing output
+
+
+
+	//end sim block
+	initial begin
+        #50 
+        if (err==0)
+          $display("***TEST PASSED! :) ***");
+        $finish;
+      end
+
+
+	//instantiation
+	lights top (.clk (clk),.rst (rst),.colour (colour));
+
+end module
