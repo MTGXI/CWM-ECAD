1833c84294c382e589cda7d7d1464f6d4d296a42
Cloud User <centos@ip-172-31-3-169.eu-west-2.compute.internal>
2021-06-09 11:46:25 +0000

    first run through

--- Ex6/top.v
+++ Ex6/top.v
@@ -14,4 +14,23 @@
 //           rgb [23:0]
 //
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 100ps
 
+module RGB(clk,enable,colour,rgb);
+
+	//ports
+	input clk,enable;
+	input [2:0] colour;
+	output [23:0] rgb;
+
+	
+	mybram rgb_mem (
+	  .clka(clk),    // input wire clka
+	  .ena(enable),      // input wire ena
+	  .wea(wea),      // input wire [0 : 0] wea
+	  .addra(colour),  // input wire [2 : 0] addra
+	  .dina(24'b0),    // input wire [23 : 0] dina
+	  .douta(rgb)  // output wire [23 : 0] douta
+	);
+
+endmodule
--- Ex6/top_tb.v
+++ Ex6/top_tb.v
@@ -6,4 +6,60 @@
 // Description: A testbench module to test Ex6 - RGB Colour Converter
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 100ps
 
+module top_tb(
+	);
+
+	//parameters
+	parameter CLK_PERIOD = 10;
+
+	//registers
+	reg clk,err,enable;
+	reg [2:0] colour;
+	wire [23:0] rgb;
+	
+
+	//creating the clock
+	initial begin
+       clk = 1'b0;
+       forever
+         #(CLK_PERIOD/2) clk=~clk;
+     end
+
+	//logic and testing output
+	
+	initial begin
+		err = 0;
+		enable = 0;
+		colour = 3'b0;
+		
+		enable = 1; //tells to look up a colour
+		forever begin
+			#CLK_PERIOD
+			$display("colour=%d, rgb=%d", colour,rgb);
+			
+			colour = colour + 3'd1;
+		end
+
+		
+	end
+
+	//end sim block
+	initial begin
+        #500 
+        if (err==0)
+          $display("***TEST PASSED***");
+        $finish;
+      end
+
+
+	//instantiation
+	RGB top (
+		.clk(clk),
+		.enable (enable),
+		.colour (colour),
+		.rgb (rgb)
+		);
+
+endmodule
